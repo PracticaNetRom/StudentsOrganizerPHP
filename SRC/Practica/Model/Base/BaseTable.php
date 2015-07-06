925c3c6fa4e2d6663341d423da1a3a6e80405dfd
Ghituleasa Claudiu <ghituleasa.claudiu@yahoo.com>
2015-07-06 17:19:35 +0300

    day 3

---
+++ SRC/Practica/Model/Base/BaseTable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laboratory1
+ * Date: 7/6/2015
+ * Time: 4:35 PM
+ */
+
+namespace Practica\Model\Base;
+
+
+abstract class BaseTable {
+    abstract public function getTableName();
+    public function findAll ()
+    {
+        try {
+            $dbh = \Practica\Connections\Database::getInstance();
+            $query= 'SELECT * from '.$this->getTableName();
+            $statement= $dbh->prepare($query);
+            $statement-> setFetchMode(\PDO::FETCH_CLASS,str_replace("Table","",get_class($this)));
+            $statement->execute();
+            return $statement->fetchAll();
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
--- web/index.php
+++ web/index.php
@@ -8,4 +8,11 @@ $app->get('/hello', function () {
     return 'Hello!';
 });
 
+$app->get('/Students', function () {
+    $StudentTable= new \Practica\Model\StudentTable();
+    var_dump($StudentTable->findAll());
+
+    return 'Welcome to Students!';
+});
+
 $app->run();
