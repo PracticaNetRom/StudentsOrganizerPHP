ee1db62595b325a0c67f52102b8d0fab03a53c12
stefaniaelena <stefy.stefanya@yahoo.com>
2015-07-06 17:20:40 +0300

    sesion3

--- web/index.php
+++ web/index.php
@@ -8,4 +8,13 @@ $app->get('/hello', function () {
     return 'Hello!';
 });
 
+$app->get('/student',function(){
+    $studentTable = new \Practica\Model\StudentTable();
+    var_dump($studentTable->findAll());
+
+    return 'student';
+});
+
 $app->run();
+
+
