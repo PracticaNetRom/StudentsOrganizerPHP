4f442ed510be82ef36757101e0e0bf19b4872d0d
alexandrugabriel <stoenescualexandrugabriel@yahoo.com>
2015-07-08 14:43:27 +0300

    tema

--- web/index.php
+++ web/index.php
@@ -6,16 +6,68 @@ $app = new Silex\Application();
 
 $app->get('/hello', function () {
     return 'Hello!';
+});
 
+$app->get('/DeparOrganizer', function () {
+    $DeparOrganizerTable= new \Practica\Model\DeparOrganizer\DeparOrganizerTable();
+    var_dump($DeparOrganizerTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/Departaments', function () {
+    $DepartamentTable= new \Practica\Model\Departament\DepartamentTable();
+    var_dump($DepartamentTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/Events', function () {
+    $EventTable= new \Practica\Model\Event\EventTable();
+    var_dump($EventTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/Faculties', function () {
+    $FacultyTable= new \Practica\Model\Faculty\FacultyTable();
+    var_dump($FacultyTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/FacultyAndStudent', function () {
+    $FacultyAndStudentTable= new \Practica\Model\FacultyAndStudent\FacultyAndStudentTable();
+    var_dump($FacultyAndStudentTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/PhoneNumber', function () {
+    $PhoneNumberTable= new \Practica\Model\PhoneNumber\PhoneNumberTable();
+    var_dump($PhoneNumberTable->findAll());
+
+    return 'The end!';
+});
+
+$app->get('/PhoneNumberType', function () {
+    $PhoneNumberTypeTable= new \Practica\Model\PhoneNumberType\PhoneNumberTypeTable();
+    var_dump($PhoneNumberTypeTable->findAll());
+
+    return 'The end!';
 });
 
 
-$app->get('/students', function () {
-
-$StudentTable = new \Practica\Model\StudentTable();
+$app->get('/Students', function () {
+    $StudentTable= new \Practica\Model\Student\StudentTable();
     var_dump($StudentTable->findAll());
-    return 'Students Hello';
+
+    return 'The end!';
 });
+
+
+
+
 
 
 $app->run();
