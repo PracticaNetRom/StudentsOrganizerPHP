81ec37a4db2372b74f90dceb1e1735f83ee6cecb
LazarCostinel <costinellazar@gmail.com>
2015-07-08 17:11:16 +0300

    sesion4

---
+++ src/practica/Model/Student.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laboratory1
+ * Date: 7/6/2015
+ * Time: 4:56 PM
+ */
+
+namespace Practica\Model;
+
+
+class Student {
+    private $id;
+    private $first_name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @param mixed $first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBirthDay()
+    {
+        return $this->birth_day;
+    }
+
+    /**
+     * @param mixed $birth_day
+     */
+    public function setBirthDay($birth_day)
+    {
+        $this->birth_day = $birth_day;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+    private $last_name;
+    private $gender;
+    private $birth_day;
+    private $email;
+}
