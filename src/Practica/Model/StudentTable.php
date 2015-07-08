a62fe1c57b370273405f3c49e04e2c51b1738305
Voicu Florentin <voicu_florentin@yahoo.com>
2015-07-08 21:06:57 +0300

    update

---
+++ src/Practica/Model/StudentTable.php
@@ -0,0 +1,13 @@
+<?php
+namespace Practica\Model;
+
+use Practica\Model\Base\BaseTable;
+
+class StudentTable extends BaseTable {
+
+    public function getTableName()
+    {
+        return 'students';
+
+    }
+}
