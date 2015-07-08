4f729bba3ed8077430e06beca61ab2e0261133a7
LazarCostinel <costinellazar@gmail.com>
2015-07-08 17:03:46 +0300

    sesion4

--- web/index.php
+++ web/index.php
@@ -1,8 +1,21 @@
 <?php
 
+
 require_once __DIR__.'/../vendor/autoload.php';
+use Silex\Provider\FormServiceProvider;
+
+
+
+
 
 $app = new Silex\Application();
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../src/Practica/Views',
+));
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('en'),
+));
 
 $app->get('/hello', function () {
     return 'Hello!';
@@ -14,6 +27,20 @@ $app->get('/student',function(){
 
     return 'student';
 });
+
+$app->match('/{ControllerName}/{ActionName}',function($ControllerName,$ActionName) use($app){
+
+    $ControllerName=ucfirst($ControllerName);
+    $class='\\Practica\\Controller\\'.$ControllerName.'Controller';
+    $Method = $ActionName . 'Action';
+    $Controller = new $class;
+    return $Controller->$Method($app['request'],$app);
+});
+
+
+
+
+$app ['debug'] =true;
 
 $app->run();
 
