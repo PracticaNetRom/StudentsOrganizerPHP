9a176c6e0218551ae409177771fb6dfef51443d7
alexandrugabriel <stoenescualexandrugabriel@yahoo.com>
2015-07-08 17:04:25 +0300

    season 4

--- web/index.php
+++ web/index.php
@@ -2,7 +2,20 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Silex\Provider\FormServiceProvider;
+
 $app = new Silex\Application();
+
+$app->register(new FormServiceProvider());
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../src/Practica/Views',
+));
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('en'),
+));
+
+
 
 $app->get('/hello', function () {
     return 'Hello!';
@@ -16,6 +29,16 @@ $StudentTable = new \Practica\Model\StudentTable();
     var_dump($StudentTable->findAll());
     return 'Students Hello';
 });
+$app->match("/{controllerName}/{actionName}",function($controllerName, $actionName)
+use ($app)
+{
+    $controllerName = ucfirst($controllerName);
+    $class = '\\Practica\\Controller\\'.$controllerName.'Controller';
+    $method = "{$actionName}Action";
+    $controller = new $class;
+    return $controller->$method ($app ['request'],$app );
+});
 
 
+$app ['debug'] = true;
 $app->run();
