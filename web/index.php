5ce3a799a63e06ee0cec21ec567d2f630a47e3c4
Ghituleasa Claudiu <ghituleasa.claudiu@yahoo.com>
2015-07-10 14:10:54 +0300

    Revert "tema2"
    This reverts commit 943bc28bff672e503f3eb204f2ac4d6eeaa14f24.

--- web/index.php
+++ web/index.php
@@ -98,17 +98,6 @@ $app->match('/{controllerName}/{actionName}',function($controllerName,$actionNam
     return $controller->$method($app['request'],$app);
 
 });
-$app->match('/{controllerName}/{actionName}',function($controllerName,$actionName)
-use ($app)
-{
-    $controllerName=ucfirst($controllerName);
-    $class= "\\Practica\\Controllers\\{$controllerName}Controller";
-    $method= "{$actionName}Action";
-    $controller = new $class;
-    return $controller->$method($app['request'],$app);
-
-});
-
 
 $app['debug'] = true;
 $app->run();
